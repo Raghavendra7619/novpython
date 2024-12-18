c78ea30547692e596ea9412ad50fe6f41257e2b8
Raghavendra7619 <raghavendradn76@gmail.com>
2024-12-18 08:05:38 +0530

    Create aaaa.py

---
+++ aaaa.py
@@ -0,0 +1,15 @@
+while True:
+    print('1.payment failed')
+    print('profile and privacy')
+    print('money and trancfer')
+    print('exit')
+    user_inp=int(input('enter ur choice'))
+    if user_inp==1:
+        print('welcome to payment failed')
+    elif user_inp==2:
+        print('welcome to profile and privacy')
+    elif user_inp==3:
+        print('welcome to money and trancfer ')
+    elif user_inp==4:
+        print('exit')
+        break
