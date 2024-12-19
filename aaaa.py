1c7bf86b9d1e46de3cd48165cf094465100ffc26
Raghavendra7619 <raghavendradn76@gmail.com>
2024-12-19 08:04:16 +0530

    Update aaaa.py

--- aaaa.py
+++ aaaa.py
@@ -1,15 +1,17 @@
-while True:
-    print('1.payment failed')
-    print('profile and privacy')
-    print('money and trancfer')
-    print('exit')
-    user_inp=int(input('enter ur choice'))
-    if user_inp==1:
-        print('welcome to payment failed')
-    elif user_inp==2:
-        print('welcome to profile and privacy')
-    elif user_inp==3:
-        print('welcome to money and trancfer ')
-    elif user_inp==4:
-        print('exit')
-        break
+count=0
+sum=0
+product=1
+for x in range(1,10,2):
+    print(x)
+    count=count+1
+    sum=sum+x
+    product=product*x
+print('total values of odd',count)
+print('total sum of odd', sum)
+print('total values of odd',product)
+
+
+
+
+
+
