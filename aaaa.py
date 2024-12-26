4459746b4cd232f6557e50d490cd2e1a346d28b9
Raghavendra7619 <raghavendradn76@gmail.com>
2024-12-26 08:20:13 +0530

    Update aaaa.py

--- aaaa.py
+++ aaaa.py
@@ -1,5 +1,12 @@
-val='nitin'
-if val[::1]==val[::]:
-    print('palindrome')
+corr_ans=0
+wrong_=0
+print('1.what is the capital of india?')
+user_ans=input('enter ans:').lower()
+print(user_ans)
+if user_ans=='delhi':
+    corr_ans+=1
+    print('correct ans')
 else:
-    print('not palindrome')
+    wrong_ans+=1
+    print('wrong ans')
+print('percentage',((corr_ans/(corr_ans+wrong_ans))*100))
