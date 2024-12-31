bccc5b088d2b1949b0941f58077bba447cdbfe94
Raghavendra7619 <raghavendradn76@gmail.com>
2024-12-31 07:48:59 +0530

    Update aaaa.py

--- aaaa.py
+++ aaaa.py
@@ -1,12 +1,4 @@
-corr_ans=0
-wrong_=0
-print('1.what is the capital of india?')
-user_ans=input('enter ans:').lower()
-print(user_ans)
-if user_ans=='delhi':
-    corr_ans+=1
-    print('correct ans')
-else:
-    wrong_ans+=1
-    print('wrong ans')
-print('percentage',((corr_ans/(corr_ans+wrong_ans))*100))
+for col in range(5):
+    for row in range(5-row):
+        print("*",end=' ')
+    print()
