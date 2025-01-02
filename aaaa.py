1951bd9c4b7243b147d2784f7813ad20c83b02c7
Raghavendra7619 <raghavendradn76@gmail.com>
2025-01-02 08:18:08 +0530

    Update aaaa.py

---
+++ aaaa.py
@@ -0,0 +1,16 @@
+tkts=[1,2,3,4,5,]
+print('avilabe tkts',tkts)
+user_input=int(input('enter how many tkts u wanna buy'))
+for x in range(user_input):
+     tickt_num=int(input('enter tckt number'))
+     tkts.remove(tkts.num)
+print('avilabe tkts', tkts)
+
+lst=[1,2,3,4,1,2]
+#op:[1,2,3,4]
+emp_1st=[]
+for x in lst:
+    if x not in emp_1st:
+        emp_1st.append(x)
+print(emp_1st)
+
