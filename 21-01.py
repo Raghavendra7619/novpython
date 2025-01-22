183ccb24fc9e22eed9a5804f4be93f03b84fcbce
Raghavendra7619 <raghavendradn76@gmail.com>
2025-01-22 08:24:53 +0530

    Create 21-01.py

---
+++ 21-01.py
@@ -0,0 +1,14 @@
+class Car:
+    def__init__(self):
+    self.name='tata'
+    print(self.name)
+    self.cost=2
+    print(self.cost)
+    print('pgm running')
+tata=Car()
+tata
+    
+
+
+class Insta:
+    def__init__
