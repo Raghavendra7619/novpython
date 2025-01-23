ce488ca8e80edcc60f8bfa1f0aa7003d614b2f7b
Raghavendra7619 <raghavendradn76@gmail.com>
2025-01-23 08:22:12 +0530

    Update Untitled-2.py

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,8 +1,11 @@
-class instagram:
+class maths:
     def__init__(self):
-    self.sub_name:'instagram'
+    self.sub_name:'maths'
     def add(self)
     a,b=10,20
     print(a+b)
  m1=maths()
+    
+
+
     
